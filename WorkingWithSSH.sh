c1fec4efd774c345b46e436f40c80355c537463a
Onur Yüksektepeli <onuryuksektepeli@hotmail.com.tr>
2020-04-13 16:43:21 +0300

    bash comments

--- WorkingWithSSH.sh
+++ WorkingWithSSH.sh
@@ -1,17 +1,17 @@
-//look for ssh files
+#look for ssh files
 ls -al ~/.ssh
 
-//create ssh key
+#create ssh key
 ssh-keygen -t rsa -b 4096 -C "onuryuksektepeli@hotmail.com.tr" -f $HOME/.ssh/github
 
-//check ssh agent
+#check ssh agent
 eval $(ssh-agent -s)
 
-//add ssh key to agent
+#add ssh key to agent
 ssh-add ~/.ssh/github
 
-//add public key to github
+#add public key to github
 
 
-//connect to github
+##connect to github
 ssh -T git@github.com
