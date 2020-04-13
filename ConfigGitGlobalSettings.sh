018771625b7016738f1fcab26f5d2ea4524f75b3
Onur Yüksektepeli <onuryuksektepeli@hotmail.com.tr>
2020-04-13 15:30:47 +0300

    git global config and initial commands are added

---
+++ ConfigGitGlobalSettings.sh
@@ -0,0 +1,4 @@
+git config --global user.name "Onur Yüksektepeli"
+git config --global user.mail "onuryuksektepeli@hotmail.com.tr"
+git config --edit --global
+git config --global core.editor "code --wait --new-window"
---
+++ GitBasics.sh
@@ -0,0 +1,16 @@
+mkdir "d:\Repos\GithubTraining"
+cd "d:\repos\GithubTraining"
+git init
+ls -la
+git status
+code Readme.md
+//Save the file
+git status
+git add Readme.md
+git status
+git commit -m "initial commit"
+
+git add . 
+git log
+
+git commit -am "new files added"
