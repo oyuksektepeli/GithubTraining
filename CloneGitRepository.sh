9ed4e0da93d83106e3c244ebcb2695c8720c9a00
Onur Yüksektepeli <onuryuksektepeli@hotmail.com.tr>
2020-04-20 15:59:29 +0300

    clone remote repository and change some files

---
+++ CloneGitRepository.sh
@@ -0,0 +1,26 @@
+#clone remote repository
+git clone https://github.com/oyuksektepeli/GithubTraining.git
+
+#see repo directory
+ls
+
+#check repo status
+git status
+
+#add some files
+
+#see new files 
+git status
+
+#add files to repo
+git add . 
+
+#check added files tracked
+git status
+
+#commit changes 
+git commit -m "new files added"
+
+#push commit to git server
+git push origin master
+
