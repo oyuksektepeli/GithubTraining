e3f5b98569dc9261d36138f93049f571ebcc7840
Onur Yüksektepeli <onuryuksektepeli@hotmail.com.tr>
2020-05-06 17:07:02 +0300

    comments are added

--- GitBasics.sh
+++ GitBasics.sh
@@ -1,16 +1,40 @@
+#create folder for repository
 mkdir "d:\Repos\GithubTraining"
+
+#got to folder
 cd "d:\repos\GithubTraining"
+
+#initialize git repository
 git init
+
+#check repository
 ls -la
+
+#check repository status
 git status
+
+#create readme file with vscode
 code Readme.md
-//Save the file
+
+#Save the file
+
+#check the status of repository
 git status
+
+#add file to staging
 git add Readme.md
+
+#chekc statusp of repository
 git status
+
+#commit the changes
 git commit -m "initial commit"
 
+#add all local change to repository
 git add . 
+
+#check repository logs
 git log
 
+#commit new files
 git commit -am "new files added"
