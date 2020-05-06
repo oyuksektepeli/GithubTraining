06a38be2527575c2889d9c3bf257c38adec30b26
Onur Yüksektepeli <onuryuksektepeli@hotmail.com.tr>
2020-05-07 01:25:35 +0300

    branching added

---
+++ GitBranching.sh
@@ -0,0 +1,9 @@
+#create a branch
+git branch branch-name
+
+#switch to branch, if not exists it also create branch
+git checkout -b branch-name
+
+#push branch to github
+git push -u origin branch-name
+
