985875b3ab26fe2bd7e40897d450961d56610878
Onur Yüksektepeli <onuryuksektepeli@hotmail.com.tr>
2020-05-11 12:59:41 +0300

    push local repository to remote

---
+++ GitPush.sh
@@ -0,0 +1,17 @@
+#initialize repository
+git init
+
+#add files to repository
+git add . or git add ['filename']
+
+#commit files
+git commit -m "My first File"
+
+#add remote origin
+git remote add origin https://github.com/yourusername/your-repo-name.git
+
+#pull first remote
+git pull origin master
+
+#git push to remote
+git push origin master
