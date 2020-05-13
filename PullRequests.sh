649539af82b293e3f9c07ab559487a9649006a21
Onur Yüksektepeli <onuryuksektepeli@hotmail.com.tr>
2020-05-13 14:12:03 +0300

    pull request commands added

---
+++ PullRequests.sh
@@ -0,0 +1,23 @@
+#create a new code in new branch
+git checkout -b new-branch-name
+
+#create branch on remote (github)
+git push --set-upstream origin new-branch-name
+
+#write some code and add some files to repository
+git add file-name
+git add file-name
+
+#to add all files in current context
+git add .
+
+#get status of repository
+get status
+
+#commit changes to new branch
+git commit -am "commit message" 
+
+#add commits to remote 
+git push 
+
+#create pull request from UI
